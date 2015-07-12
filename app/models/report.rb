5813d2eec1512e4f0e5e1690ad2b0dcb9818cb5b
Artem Kulakov <artem.kulakov@gmail.com>
2015-07-12 15:40:38 +0400

    Face page caching

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 class Report < ActiveRecord::Base
   validates :company, :period, :revenues, presence: true
   validates :revenues, numericality: {greater_than_or_equal_to: 0}
+  
+  def self.latest
+    Report.order(:updated_at).last
+  end
 end
