293188a3abe7dda62b0830e72568d69ad8f51d8a
Artem Kulakov <artem.kulakov@gmail.com>
2015-07-17 13:19:01 +0400

    Benchmark Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Benchmark::Application.config.secret_key_base = '2b0ca0d7515998cbd42cfa58d6ec35532cca46f55812a798730e9c3ad48ed9fc1174dbe7794776ffd6ff340c29d3864fe319575ddb1bb7eaae3961cd37850259'
