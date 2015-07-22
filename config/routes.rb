9937f1cc831eb916032dc7dd6bbd24c1955cb6ab
Artem Kulakov <artem.kulakov@gmail.com>
2015-07-22 06:53:15 +0400

    Industry created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Benchmark::Application.routes.draw do
+  resources :industries
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
