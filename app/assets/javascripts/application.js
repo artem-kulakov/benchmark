040a87da9fda9794b3817752acbd95ff0d6941d2
Artem Kulakov <artem.kulakov@gmail.com>
2015-10-03 00:13:10 +0400

    New Bootstrap installed:
    Gem-Bootstrap removed
    Manual Bootstrap installed
    Now glyphicons and css-tree work
    scaffolds.css.sass deleted (it made text in button grey)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui/sortable
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap.min
