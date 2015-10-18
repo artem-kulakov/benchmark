20258a0fd3c990c902e0c361846490dcde85bfbe
Artem Kulakov <artem.kulakov@gmail.com>
2015-10-18 11:06:28 +0400

    reports#index view refactored

--- app/models/report.rb
+++ app/models/report.rb
@@ -14,4 +14,38 @@ class Report < ActiveRecord::Base
   has_one :industry, through: :company
   has_many :indicators, through: :industry
   has_many :formulas, through: :indicators
+  
+  def company_title
+    company.title
+  end
+  
+  # The most rated author of this report
+  def author_id
+    users.order("rating DESC").first.id
+  end
+  
+  # Report version id
+  def version_id
+    versions.where(user_id: self.author_id).pluck(:id).pop
+  end
+  
+  # Value of indicator
+  def indicator_value(indicator)
+    values.where(indicator_id: indicator.id, version_id: self.version_id).pluck(:value).pop
+  end
+  
+  # User's email
+  def author_email
+    users.last.email
+  end
+  
+  # Checker's id
+  def checker_id
+    approvals.last.user.id
+  end
+  
+  # Checker's email
+  def checker_email
+    approvals.last.user.email
+  end
 end
